65c522a77864093cd51a5febe8f514133009b920
David Clarke <107880611+CDGADonc@users.noreply.github.com>
2022-08-15 12:58:23 +0100

    initial

--- app.js
+++ app.js
@@ -1,5 +1,30 @@
 const {buildDB} = require('./db/populateDataBase')
+const express = require('express')
+const {Cheese} = require('./models')
+const app = express()
 buildDB()
 
 
+app.get('/feta', async(req,res)=>{
+const queriedCheese = await Cheese.findOne({where: {title: 'Feta'}})
+let {title,description} = queriedCheese
+let payload = {
+    title: title,
+    description: description
+}
+res.send(payload)
+})
 
+app.get('/starts-with-c', async(req,res) =>{
+    const dbQuery= await Cheese.findAll()
+    let startsWithC = dbQuery .filter((cheese)=> {
+        if (cheese.title[0] === 'C') {
+            return true
+        }
+    })
+    
+})
+
+app.listen(3000,()=>{
+    console.log('Server running on http://localhost:3000')
+})
