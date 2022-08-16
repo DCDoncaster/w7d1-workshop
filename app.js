c20c7a01316186e02a1c258cc61229ea1a166ccf
David Clarke <107880611+CDGADonc@users.noreply.github.com>
2022-08-16 13:38:47 +0100

    Route and  Query Paramaters

--- app.js
+++ app.js
@@ -1,19 +1,29 @@
 const {buildDB} = require('./db/populateDataBase')
 const express = require('express')
 const {Cheese} = require('./models')
+const { reset } = require('nodemon')
 const app = express()
 buildDB()
 
 
-app.get('/feta', async(req,res)=>{
-const queriedCheese = await Cheese.findOne({where: {title: 'Feta'}})
+app.get('/cheeses/:cheese', async(req,res)=>{
+
+let newString = req.params.cheese[0].toUpperCase() + req.params.cheese.slice(1).toLowerCase()
+
+const queriedCheese = await Cheese.findOne({where: {title: newString}})
+if (!queriedCheese){
+    res.send("Sorry we don't have that cheese")
+}
+else {
+    
 let {title,description} = queriedCheese
 let payload = {
     title: title,
     description: description
 }
 res.send(payload)
-})
+
+}})
 
 app.get('/starts-with-c', async(req,res) =>{
     const dbQuery= await Cheese.findAll()
@@ -22,8 +32,24 @@ app.get('/starts-with-c', async(req,res) =>{
             return true
         }
     })
-    
+    res.send(200)
 })
+
+app.get('/cheeses', async (req,res)=>{
+    const dbQuery = await Cheese.findAll()
+    let startsWithAlpha = dbQuery.filter((cheese)=> {
+        if (cheese.title[0] === req.query.startswith.toUpperCase()){
+            return true
+        }
+    })
+    if(startsWithAlpha.length ===0){
+        res.send("Sorry, no mathces.")
+        return
+    }
+    console.log(startsWithAlpha.length)
+    res.send(startsWithAlpha)
+})
+
 
 app.listen(3000,()=>{
     console.log('Server running on http://localhost:3000')
